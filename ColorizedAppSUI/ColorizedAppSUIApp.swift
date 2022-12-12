a30ed451b1d414424d6b1b013ba56a3281f71315
Denis Balandin <doublestar2013@yandex.ru>
2022-12-12 20:55:16 +0300

    Initial Commit

---
+++ ColorizedAppSUI/ColorizedAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSUIApp.swift
+//  ColorizedAppSUI
+//
+//  Created by Denis on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
